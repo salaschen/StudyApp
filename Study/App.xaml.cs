efc0edd6e2058b1597531b181fc449125367ac77
salaschen <6499430+salaschen@users.noreply.github.com>
2023-11-12 16:30:38 +1100

    Initial commit: Project created.

---
+++ Study/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Study;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Study/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Study;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
